11f08c53809da672ef405e34a525492980ba8e8c
ILYA <tuarone@gmail.com>
2023-08-06 01:42:13 +0200

    added a form validation test on the registration page

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,6 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+from .pages.locators import LoginPageLocators
 
 def test_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -11,3 +13,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+def test_should_be_login_page(browser):
+    login_page = LoginPage(browser, LoginPageLocators.LOGIN_URL)
+    login_page.open()
+    login_page.should_be_login_page()
