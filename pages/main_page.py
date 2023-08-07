5273786fe8154ea66994eeccc7afdf89dc64e910
ILYA <tuarone@gmail.com>
2023-08-07 23:12:32 +0200

    pass

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 class MainPage(BasePage):
+    pass
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
