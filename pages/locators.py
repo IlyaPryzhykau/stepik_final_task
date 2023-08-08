4cc4ffd9e175c89de974691a26de00064f635d9d
ILYA <tuarone@gmail.com>
2023-08-09 00:32:17 +0200

    add BasketPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    MAIN_PAGE_LINK = 'http://selenium1py.pythonanywhere.com/'
+    BUTTON_VIEW_BASKET = (By.CSS_SELECTOR, '.btn-group > a')
 
 
 class LoginPageLocators():
@@ -23,8 +25,7 @@ class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
-
-
-
-
+class BasketPageLocators():
+    BASKET_TOTAL = (By.CSS_SELECTOR, '#basket_totals')
+    TEXT_EMPTY_BASKET = (By.CSS_SELECTOR, '#content_inner > p')
 
