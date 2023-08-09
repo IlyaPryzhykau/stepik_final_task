639dd5e950d9035a817adcd454aa58e38711c267
ILYA <tuarone@gmail.com>
2023-08-09 04:26:17 +0200

    final push

--- test_product_page.py
+++ test_product_page.py
@@ -21,6 +21,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         page = ProductPage(browser, ProductPageLocators.PRODUCT_URL)
         page.open()
@@ -30,17 +31,16 @@ class TestUserAddToBasketFromProductPage():
 
 
 
-initial_url = ProductPageLocators.PRODUCT_URL
-urls = [f"{initial_url}/?promo=offer{i}" for i in range(10)]
+# initial_url = ProductPageLocators.PRODUCT_URL
+# urls = [f"{initial_url}/?promo=offer{i}" for i in range(10)]
 
-
-@pytest.mark.parametrize('link', urls)
-@pytest.mark.xfail(reason="fixing this bug right now")
-def test_guest_can_add_product_to_basket(browser, link):
-    product_page = ProductPage(browser, link)
+@pytest.mark.need_review
+# @pytest.mark.parametrize('link', urls)   ----- Убрал для упрощения проверки
+# @pytest.mark.xfail(reason="fixing this bug right now")
+def test_guest_can_add_product_to_basket(browser):
+    product_page = ProductPage(browser, ProductPageLocators.PRODUCT_URL)
     product_page.open()
     product_page.should_be_item_add_to_basket()
-    product_page.solve_quiz_and_get_code()
     product_page.should_be_name_match()
     product_page.should_be_price_match()
 
@@ -65,18 +65,18 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.should_disappear_element()
 
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
+    page = BasketPage(browser, ProductPageLocators.PRODUCT_URL2)
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
+    page = BasketPage(browser, ProductPageLocators.PRODUCT_URL2)
     page.open()
     page.go_to_login_page()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = BasketPage(browser, ProductPageLocators.PRODUCT_URL)
     page.open()
