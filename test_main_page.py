9ef54b2a4595cbb8d679048e83390cd3f402a4d7
ILYA <tuarone@gmail.com>
2023-08-09 02:12:08 +0200

    add pytest.mark

--- test_main_page.py
+++ test_main_page.py
@@ -1,21 +1,22 @@
-from .pages.main_page import MainPage
+import pytest
 from .pages.login_page import LoginPage
 from .pages.locators import LoginPageLocators
 from .pages.locators import MainPageLocators
 from .pages.basket_page import BasketPage
 import time
 
-def test_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        page = LoginPage(browser, MainPageLocators.MAIN_PAGE_LINK)
+        page.open()
+        page.go_to_login_page()
+    def test_guest_should_see_login_link(self, browser):
+        page = LoginPage(browser, MainPageLocators.MAIN_PAGE_LINK)
+        page.open()
+        page.should_be_login_link()
+
 
 def test_should_be_login_page(browser):
     login_page = LoginPage(browser, LoginPageLocators.LOGIN_URL)
